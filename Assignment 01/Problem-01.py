fc20c5225a6a4f7b7a760c1594d8dc6c1fa71fff
SanimYousuf <sanimyousuf02@gmail.com>
2024-09-19 10:55:52 +0600

    solved

--- Assignment 01/Problem-01.py
+++ Assignment 01/Problem-01.py
@@ -1,1 +1,28 @@
-print("Hehe Boi, this is my new py repo!")
+def balancedSplit(str):
+    c = ans = 0
+    result = []
+    temp = ""
+
+    for char in str:
+
+        temp += char
+
+        if char == 'L':
+            c += 1
+
+        else:
+            c -= 1
+
+        if c == 0:
+            ans += 1
+            result.append(temp)
+            temp = ""
+
+    print(ans)
+    for balanced_str in result:
+        print(balanced_str)
+
+str = input()
+
+balancedSplit(str)
+    
